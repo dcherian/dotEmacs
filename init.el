e7335f0d6abb6c6ab701a5600783b7d3fdb7d405
deepak <dpak.cherian@gmail.com>
2017-03-22 13:31:08 -0600

    Turn off GUI decorations even if not window-system.
    Fixes things for emacsclient.

--- init.el
+++ init.el
@@ -42,10 +42,10 @@
 (define-key dc/toggle-map "w" #'whitespace-mode)
 
 (require 'dc-theme)
-(when window-system
-  (tool-bar-mode 0)
-  (menu-bar-mode 0)
-  (scroll-bar-mode 0))
+
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
 
 (setq inhibit-startup-message t
       initial-scratch-message ""
