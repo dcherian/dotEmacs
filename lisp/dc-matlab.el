f5b29ffc51a44b6ef77b1c53132a92c5161433f6
deepak <dpak.cherian@gmail.com>
2017-03-24 12:01:23 -0600

    matlab-mode still does not work :(

--- lisp/dc-matlab.el
+++ lisp/dc-matlab.el
@@ -2,6 +2,8 @@
 ;; My matlab-mode configuration
 ;; - Deepak Cherian
 
+(defvar default-fill-column 100)
+
 (use-package matlab-mode
   :ensure t
   :mode "\\.m$"
@@ -9,20 +11,21 @@
 	      ("C-c C-m" . matlab-shell)
 	      ("C-c C-c" . matlab-shell-run-region-or-line)
 	      ("C-c C-a" . matlab-shell-run-cell)
-	      ("C-c C-o" . dc/matlab-shell-other-window))
-  :config
-  (setq-default matlab-shell-command "/Applications/MATLAB_R2016a.App/bin/matlab"
-		matlab-indent-function-body t   ; if you want function bodies indented
-		matlab-functions-have-end t
-		matlab-verify-on-save-flag nil
-		matlab-shell-command-switches '("-nodesktop -nosplash")
-		matlab-mode-verify-fix-functions nil)
+	      ("C-c C-o" . dc/matlab-shell-other-window)))
 
-  (defun dc/matlab-shell-other-window ()
-    (interactive)
-    (other-window 1)
-    (matlab-shell))
+(setq-default matlab-shell-command "matlab"
+	      matlab-indent-function-body t   ; if you want function bodies indented
+	      matlab-functions-have-end t
+	      matlab-verify-on-save-flag nil
+	      matlab-shell-command-switches '("-nodesktop -nosplash")
+	      matlab-mode-verify-fix-functions nil
+	      matlab-shell-history-file "~/.matlab/R2016b/history.m")
 
-  (add-hook 'matlab-shell-hook 'comint-read-input-ring))
+(defun dc/matlab-shell-other-window ()
+  (interactive)
+  (other-window 1)
+  (matlab-shell))
+
+(add-hook 'matlab-shell-hook 'comint-read-input-ring)
 
 (provide 'dc-matlab)
