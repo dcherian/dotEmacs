3f793ed79a446b8cd3f11610bbfcad62869ebe20
deepak <dpak.cherian@gmail.com>
2017-09-11 11:18:25 -0700

    Website improvements.
    1. Icons to external html file.
    2. Add CC-BY Attribution footer.

--- lisp/dc-website.el
+++ lisp/dc-website.el
@@ -2,6 +2,20 @@
 (require 'ox-html)
 (require 'ox-publish)
 
+(setq website-footer
+      "<a rel=\"license\" href=\"http://creativecommons.org/licenses/by/4.0/\"><use xlink:href='#cc-by'></use></svg></a><br />This work is licensed under a <a rel=\"license\" href=\"http://creativecommons.org/licenses/by/4.0/\">Creative Commons Attribution 4.0 International License</a>.")
+
+(setq website-head
+      (concat
+       "<link rel=\"stylesheet\" href=\"./website.css\" type=\"text/css\" />\n"
+       "<link href=\"https://fonts.googleapis.com/css?family=Fira+Sans:300,400,500\" rel=\"stylesheet\">"))
+
+(setq website-header-file "~/website/org/website-icons.html")
+(defun website-header (arg)
+  (with-temp-buffer
+    (insert-file-contents website-header-file)
+    (buffer-string)))
+
 (setq org-publish-project-alist 'nil)
 (setq org-publish-project-alist
       '(("website" :components ("pages" "blog-static"))
@@ -9,15 +23,17 @@
 	 :base-directory "~/website/org/"
 	 :base-extension "org"
 	 :recursive t
-	 :html-head "<link rel=\"stylesheet\" href=\"./website.css\" type=\"text/css\" /> <link href=\"https://fonts.googleapis.com/css?family=Fira+Sans:300,400,500\" rel=\"stylesheet\">"
-	 :html-postamble nil
+	 :html-head "<link rel=\"stylesheet\" href=\"./website.css\" type=\"text/css\" />\n<link href=\"https://fonts.googleapis.com/css?family=Fira+Sans:300,400,500\" rel=\"stylesheet\">"
+	 :html-preamble website-header
+	 :html-postamble "<div align=\"center\"><a rel=\"license\" class=\"cc-logo\" href=\"http://creativecommons.org/licenses/by/4.0/\"><svg class=\"cc-logo\"><use xlink:href='#icon-cc-by'></use></svg></a><br />This work is licensed under a <a rel=\"license\"  href=\"http://creativecommons.org/licenses/by/4.0/\">Creative Commons Attribution 4.0 International License</a>.</div>"
 	 :creator-info nil
-	 :author nil
-	 :html-doctype "xhtml5"
+	 :author "Deepak Cherian"
+	 :html-doctype "html5"
 	 :html-html5-fancy t
 	 :html-head-include-scripts nil
 	 :html-head-include-default-style nil
 	 :html-validation-link nil
+	 :html-container "section"
 	 :publishing-directory "~/website/publish/"
 	 :publishing-function org-html-publish-to-html)
 
