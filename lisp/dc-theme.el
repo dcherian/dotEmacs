aae8e8f18cb6d0fdead7ce00e0860f89a194855c
deepak <dpak.cherian@gmail.com>
2017-11-06 13:49:39 -0800

    Condense my theme changes into functions dc/org-theme dc/theme-changes

--- lisp/dc-theme.el
+++ lisp/dc-theme.el
@@ -12,24 +12,8 @@
                            ("19:30" . solarized-dark)))
   (circadian-setup))
 
-(set-face-attribute 'font-lock-constant-face nil
-		    :bold nil)
-
-(set-face-attribute 'font-lock-builtin-face nil
-		    :foreground "#b58900"
-		    :bold nil)
-
-(set-face-attribute 'default nil
-		    :foreground "#21505a"
-		    :family "mononoki"
-		    :height 130)
-
 (setq-default line-spacing 6)
 (setq x-underline-at-descent-line nil)
-(set-face-attribute 'tooltip nil
-		    :inherit 'default
-		    :foreground "#586e75"
-		    :background "#eee8d5")
 
 (use-package auto-dim-other-buffers
   :config
@@ -40,6 +24,85 @@
 
   (add-hook 'after-init-hook (lambda ()
 			       (auto-dim-other-buffers-mode t))))
+
+(defun dc/org-theme ()
+  (interactive)
+  ;; org-faces
+  (set-face-attribute 'org-level-1 nil
+		      :inherit 'outline-1 :height 1.25)
+  (set-face-attribute 'org-level-2 nil
+		      :inherit 'outline-2 :height 1.2)
+  (set-face-attribute 'org-level-3 nil
+		      :inherit 'outline-3 :height 1.15)
+  (set-face-attribute 'org-link nil
+		      :inherit 'org-link
+		      :foreground nil) ; links are only underlined
+  ;; footnotes shouldn't be highlighted
+  (set-face-attribute 'org-footnote nil
+		      :foreground nil
+		      :underline nil
+		      :inherit '(font-lock-comment-face org-foreground))
+  (set-face-attribute 'org-checkbox nil
+		      :inherit '(font-lock-comment-face)
+		      :background nil
+		      :weight 'light
+		      :box nil)
+  (set-face-attribute 'org-todo nil
+		      :weight 'normal)
+  (set-face-attribute 'org-done nil
+		      :weight 'normal)
+  (set-face-attribute 'org-block nil
+		      :foreground nil
+		      :background nil)
+  (set-face-attribute 'org-target nil
+		      :foreground "#586e75"
+		      :background nil)
+  (set-face-attribute 'org-table nil
+		      :family "Ubuntu Mono"
+		      :background nil)
+  (set-face-attribute 'org-date nil
+		      :foreground nil
+		      :inherit 'org-link)
+  (set-face-attribute 'org-latex-and-related nil
+		      :foreground "#268bd2")
+  (set-face-attribute 'org-tag nil
+		      :height 0.7
+		      :inherit '(font-lock-comment-face org-foreground))
+  (set-face-attribute 'org-ref-cite-face nil
+		      :inherit 'org-link
+		      :foreground nil)
+  (set-face-attribute 'org-ref-ref-face nil
+		      :inherit 'org-ref-cite-face
+		      :foreground nil)
+  (set-face-attribute 'org-meta-line nil
+		      :height 0.85))
+
+(defun dc/theme-changes ()
+  (interactive)
+  (auto-dim-other-buffers-mode)
+  (sml/setup)
+  (smart-mode-line-enable)
+  (dc/org-theme)
+
+  ;; font changes
+  (set-face-attribute 'font-lock-constant-face nil
+		      :bold nil)
+
+  (set-face-attribute 'font-lock-builtin-face nil
+		      :foreground "#b58900"
+		      :bold nil)
+
+  (set-face-attribute 'default nil
+		      :foreground "#21505a"
+		      :family "mononoki"
+		      :height 130)
+
+  (set-face-attribute 'tooltip nil
+		      :inherit 'default
+		      :foreground "#586e75"
+		      :background "#eee8d5"))
+
+(dc/theme-changes)
 
 ;; modeline options
 (use-package smart-mode-line
