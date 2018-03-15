c8d7588bd05d9562db3acfd4ce64d38a93234eb0
dcherian <dcherian@mit.edu>
2018-03-15 09:11:55 -0700

    Company mode fixes
    1. Use company-yasnippet backend.
    2. Reorder backends
    3. M-<number> works like ora-number stuff, so lets just use that.

--- lisp/dc-company.el
+++ lisp/dc-company.el
@@ -3,17 +3,19 @@
   :ensure t
   :config
   ;; Enable company-mode globally.
-  (global-company-mode)
+  (add-hook 'after-init-hook 'global-company-mode)
   ;; Except when you're in term-mode.
   (setq company-global-modes '(not term-mode))
 
-  (setq company-minimum-prefix-length 2
+  (setq company-minimum-prefix-length 3
         company-selection-wrap-around t
         company-show-numbers t
         company-tooltip-align-annotations t
         company-require-match nil
+	company-dabbrev-code-other-buffers t
         company-dabbrev-downcase nil
 	company-dabbrev-ignore-case nil)
+
   (setq company-idle-delay 0.6)
 
   (define-key company-active-map [tab] 'company-complete-common-or-cycle)
@@ -24,60 +26,23 @@
   (setq company-transformers '(company-sort-by-occurrence))
 
   (setq company-backends
-	'((company-files company-elisp company-semantic company-files company-dabbrev-code company-gtags company-etags company-keywords company-dabbrev)))
-
-  (defvar company-mode/enable-yas t
-    "Enable yasnippet for all backends.")
-
-  (defun company-mode/backend-with-yas (backend)
-    (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
-	backend
-      (append (if (consp backend) backend (list backend))
-	      '(:with company-yasnippet))))
-
-  (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
-
-  ;; https://oremacs.com/2017/12/27/company-numbers/
-  (let ((map company-active-map))
-    (mapc
-     (lambda (x)
-       (define-key map (format "%d" x) 'ora-company-number))
-     (number-sequence 0 9))
-    (define-key map " " (lambda ()
-                          (interactive)
-                          (company-abort)
-                          (self-insert-command 1)))
-    (define-key map (kbd "<return>") nil))
-
-  (defun ora-company-number ()
-  "Forward to `company-complete-number'.
-
-Unless the number is potentially part of the candidate.
-In that case, insert the number."
-  (interactive)
-  (let* ((k (this-command-keys))
-         (re (concat "^" company-prefix k)))
-    (if (cl-find-if (lambda (s) (string-match re s))
-                    company-candidates)
-        (self-insert-command 1)
-      (company-complete-number (string-to-number k)))))
-
-  (add-hook 'after-init-hook 'global-company-mode))
-
-(use-package helm-company
-  :disabled t
-  :config
-  (define-key company-mode-map (kbd "C-'") 'helm-company)
-  (define-key company-active-map (kbd "C-'") 'helm-company))
+	'((company-capf
+	   company-elisp
+	   company-semantic
+	   company-dabbrev-code
+	   company-gtags
+	   company-etags
+	   company-keywords
+	   company-files
+	   company-yasnippet
+	   company-dabbrev))))
 
 (use-package company-math
   :after (company)
   :config
   (add-to-list 'company-backends 'company-math-symbols-latex)
   ;; (add-to-list 'company-backends 'company-math-symbols-unicode)
-  (setq company-math-allow-latex-symbols-in-faces t)
-  ;; (add-to-list 'company-backends 'company-math-symbols-unicode)
-  )
+  (setq company-math-allow-latex-symbols-in-faces t))
 
 ;; rank completion candidates by statistics of use
 (use-package company-statistics
@@ -85,5 +50,11 @@ In that case, insert the number."
   :after (company)
   :config
   (company-statistics-mode))
+
+(use-package company-quickhelp
+  :ensure t
+  :after company
+  :config
+  (company-quickhelp-mode))
 
 (provide 'dc-company)
