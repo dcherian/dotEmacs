cdbf58f07afb7a90ee73ea8c3199acc0dca1efbd
dcherian <dcherian@mit.edu>
2018-10-26 05:31:02 -0700

    remove dubcaps-mode. doesn't work with subword-mode

--- lisp/dc-editing.el
+++ lisp/dc-editing.el
@@ -1,26 +1,4 @@
 ;;; Code
-(defun sk/dcaps-to-scaps ()
-  "Convert word in DOuble CApitals to Single Capitals."
-  (interactive)
-  (and (= ?w (char-syntax (char-before)))
-       (save-excursion
-	 (and (if (called-interactively-p)
-		  (skip-syntax-backward "w")
-		(= -3 (skip-syntax-backward "w")))
-	      (let (case-fold-search)
-		(looking-at "\\b[[:upper:]]\\{2\\}[[:lower:]]"))
-	      (capitalize-word 1)))))
-
-(define-minor-mode sk/dubcaps-mode
-  "Toggle `sk/dubcaps-mode'.  Converts words in DOuble CApitals to
-Single Capitals as you type."
-  :init-value nil
-  :lighter (" DC")
-  (if sk/dubcaps-mode
-      (add-hook 'post-self-insert-hook #'sk/dcaps-to-scaps nil 'local)
-    (remove-hook 'post-self-insert-hook #'sk/dcaps-to-scaps 'local)))
-
-(add-hook 'text-mode-hook #'sk/dubcaps-mode)
 
 (defun narrow-or-widen-dwim (p)
   "Widen if buffer is narrowed, narrow-dwim otherwise.
