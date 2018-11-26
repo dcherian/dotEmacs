b77bbf5b25287eb81e3e53ae6055741aef3becb6
dcherian <dcherian@mit.edu>
2018-11-26 14:49:15 -0700

    Add proselint flycheck

--- init.el
+++ init.el
@@ -532,6 +532,20 @@
   :config
   (wucuo-start))
 
+(use-package flycheck
+  :config
+  (global-flycheck-mode)
+  (flycheck-define-checker proselint
+    "A linter for prose."
+    :command ("proselint" source-inplace)
+    :error-patterns
+    ((warning line-start (file-name) ":" line ":" column ": "
+	      (id (one-or-more (not (any " "))))
+	      (message) line-end))
+    :modes (text-mode markdown-mode gfm-mode org-mode))
+
+  (add-to-list 'flycheck-checkers 'proselint))
+
 (require 'dc-org)
 (require 'dc-helm)
 (require 'dc-comint)
