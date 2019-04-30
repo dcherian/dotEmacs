352640712a3c88cbd21cfc504e3efc4405bc1880
dcherian <deepak@cherian.net>
2019-04-30 12:42:09 -0600

    use-package matlab

--- lisp/dc-matlab.el
+++ lisp/dc-matlab.el
@@ -2,30 +2,29 @@
 ;; My matlab-mode configuration
 ;; - Deepak Cherian
 
-(defvar default-fill-column 200)
-
 ;; needs to be matlab not matlab-mode!
 ;; this is because matlab.el is present, not matlab-mode.el
-(require 'matlab)
+(use-package matlab
+  :init (defvar default-fill-column 200)
+  :commands dc/matlab-shell-other-window
+  :hook (matlab-shell . comint-read-input-ring)
+  :bind (:map matlab-mode-map
+	      ("C-c C-m" . matlab-shell)
+	      ("C-c C-a" . matlab-shell-run-cell)
+	      ("C-c C-c" . matlab-shell-run-region-or-line)
+	      ("C-c C-o" . dc/matlab-shell-other-window))
+  :config
+  (defun dc/matlab-shell-other-window ()
+    (interactive)
+    (other-window 1)
+    (matlab-shell))
 
-(defun dc/matlab-shell-other-window ()
-  (interactive)
-  (other-window 1)
-  (matlab-shell))
-
-(setq matlab-shell-command "matlab"
-      matlab-indent-function-body t
-      matlab-functions-have-end t
-      matlab-verify-on-save-flag nil
-      matlab-shell-command-switches '("-nodesktop -nosplash")
-      matlab-mode-verify-fix-functions nil
-      matlab-shell-history-file "~/.matlab/R2016b/history.m")
-
-(add-hook 'matlab-shell-hook 'comint-read-input-ring)
-
-(bind-key "C-c C-m" 'matlab-shell matlab-mode-map)
-(bind-key "C-c C-c" 'matlab-shell-run-region-or-line matlab-mode-map)
-(bind-key "C-c C-a" 'matlab-shell-run-cell matlab-mode-map)
-(bind-key "C-c C-o" 'dc/matlab-shell-other-window matlab-mode-map)
+  (setq matlab-shell-command "matlab"
+	matlab-indent-function-body t
+	matlab-functions-have-end t
+	matlab-verify-on-save-flag nil
+	matlab-shell-command-switches '("-nodesktop -nosplash")
+	matlab-mode-verify-fix-functions nil
+	matlab-shell-history-file "~/.matlab/R2018a/history.m"))
 
 (provide 'dc-matlab)
