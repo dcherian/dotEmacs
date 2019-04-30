9f8008d71a196649adf458c31f6ea88accf67fca
dcherian <deepak@cherian.net>
2019-04-29 21:35:51 -0600

    More edits.

--- lisp/dc-org.el
+++ lisp/dc-org.el
@@ -351,6 +351,59 @@ applying latex prettifycations in org mode buffers."
   (add-hook 'org-export-before-processing-hook 'delete-org-comments)
 
   (setq org-preview-latex-default-process 'imagemagick)
+  (setq org-format-latex-header
+	"%&~/tools/latex/preamble-memoir
+\\usepackage{fontspec}
+\\usepackage{unicode-math}
+\\setromanfont[Ligatures=TeX]{TeX Gyre Pagella}
+\\setmathfont[math-style=ISO,bold-style=ISO]{TeX Gyre Pagella Math}
+\\setmonofont{Inconsolata}
+[NO-DEFAULT-PACKAGES]
+[NO-PACKAGES]
+[NO-EXTRA]
+\\pagenumbering{gobble}
+% do not remove\n% The settings below are copied from fullpage.sty\n\\setlength{\\textwidth}{\\paperwidth}\n\\addtolength{\\textwidth}{-3cm}\n\\setlength{\\oddsidemargin}{1.5cm}\n\\addtolength{\\oddsidemargin}{-2.54cm}\n\\setlength{\\evensidemargin}{\\oddsidemargin}\n\\setlength{\\textheight}{\\paperheight}\n\\addtolength{\\textheight}{-\\headheight}\n\\addtolength{\\textheight}{-\\headsep}\n\\addtolength{\\textheight}{-\\footskip}\n\\addtolength{\\textheight}{-3cm}\n\\setlength{\\topmargin}{1.5cm}\n\\addtolength{\\topmargin}{-2.54cm}")
+
+  ;; from https://emacs.stackexchange.com/questions/30341/how-do-i-customize-the-process-that-gets-triggered-in-org-preview-latex-fragment/33172
+  (setq org-preview-latex-process-alist
+	(quote
+	 ((dvipng :programs
+		  ("lualatex" "dvipng")
+		  :description "dvi > png"
+		  :message "you need to install the programs: latex and dvipng."
+		  :image-input-type "dvi"
+		  :image-output-type "png"
+		  :image-size-adjust (1.0 . 1.0)
+		  :latex-compiler
+		  ("lualatex -output-format dvi -interaction nonstopmode -output-directory %o %f")
+		  :image-converter
+		  ("dvipng -fg %F -bg %B -D %D -T tight -o %O %f"))
+
+	  (dvisvgm :programs ("latex" "dvisvgm")
+		   :description "dvi > svg"
+		   :message
+		   "you need to install the programs: latex and dvisvgm."
+		   :use-xcolor t
+		   :image-input-type "xdv"
+		   :image-output-type "svg"
+		   :image-size-adjust (1.7 . 1.5)
+		   :latex-compiler
+		   ("xelatex -no-pdf -interaction nonstopmode -output-directory %o %f")
+		   :image-converter
+		   ("dvisvgm %f -n -b min -c %S -o %O"))
+
+	  (imagemagick
+	   :programs ("latex" "convert")
+	   :description "pdf > png"
+	   :message "you need to install the programs: latex and imagemagick."
+	   :use-xcolor t
+	   :image-input-type "pdf"
+	   :image-output-type "png"
+	   :image-size-adjust (1.5 1.5)
+	   :latex-compiler
+	   ("xelatex -pdf -interaction nonstopmode -output-directory %o %f")
+	   :image-converter
+	   ("convert -density 180 -trim -antialias %f -quality 100 %O")))))
 
   (use-package ov
     :ensure)
@@ -492,20 +545,18 @@ Argument KEY is the bibtex key."
 
 (use-package org-edit-latex
   :ensure t
-  :after (org)
-  :config
-  (org-edit-latex-mode t))
+  :after org
+  :commands org-edit-latex-mode)
 
 (use-package orgalist
   :ensure
   :after org
+  :hook ((git-commit-setup-hook . orgalist-mode)
+	 (mail-mode-hook . orgalist-mode)
+	 (message-mode-hook . orgalist-mod))
   :bind (:map dc-bindings-map
 	      (:map org-mode-map
-		    ("RET" . scimax/org-return)))
-  :config
-  (add-hook 'git-commit-setup-hook #'orgalist-mode)
-  (add-hook 'mail-mode-hook #'orgalist-mode)
-  (add-hook 'message-mode-hook #'orgalist-mode))
+		    ("RET" . scimax/org-return))))
 
 (use-package ox-gfm
   :defer
@@ -543,60 +594,6 @@ Argument KEY is the bibtex key."
   (setq org-latex-pdf-process
 	'("source ~/.bashrc; sed '/./,$!d' %f > %f.nolines; mv %f.nolines %f; latexmk %f; exiftool -overwrite_original -Producer=`git rev-parse HEAD` %b.pdf"))
   (require 'dc-ox-latex-classes))
-
-(setq org-format-latex-header
-      "%&~/tools/latex/preamble-memoir
-\\usepackage{fontspec}
-\\usepackage{unicode-math}
-\\setromanfont[Ligatures=TeX]{TeX Gyre Pagella}
-\\setmathfont[math-style=ISO,bold-style=ISO]{TeX Gyre Pagella Math}
-\\setmonofont{Inconsolata}
-[NO-DEFAULT-PACKAGES]
-[NO-PACKAGES]
-[NO-EXTRA]
-\\pagenumbering{gobble}
-% do not remove\n% The settings below are copied from fullpage.sty\n\\setlength{\\textwidth}{\\paperwidth}\n\\addtolength{\\textwidth}{-3cm}\n\\setlength{\\oddsidemargin}{1.5cm}\n\\addtolength{\\oddsidemargin}{-2.54cm}\n\\setlength{\\evensidemargin}{\\oddsidemargin}\n\\setlength{\\textheight}{\\paperheight}\n\\addtolength{\\textheight}{-\\headheight}\n\\addtolength{\\textheight}{-\\headsep}\n\\addtolength{\\textheight}{-\\footskip}\n\\addtolength{\\textheight}{-3cm}\n\\setlength{\\topmargin}{1.5cm}\n\\addtolength{\\topmargin}{-2.54cm}")
-
-;; from https://emacs.stackexchange.com/questions/30341/how-do-i-customize-the-process-that-gets-triggered-in-org-preview-latex-fragment/33172
-(setq org-preview-latex-process-alist
-      (quote
-       ((dvipng :programs
-		("lualatex" "dvipng")
-		:description "dvi > png"
-		:message "you need to install the programs: latex and dvipng."
-		:image-input-type "dvi"
-		:image-output-type "png"
-		:image-size-adjust (1.0 . 1.0)
-		:latex-compiler
-		("lualatex -output-format dvi -interaction nonstopmode -output-directory %o %f")
-		:image-converter
-		("dvipng -fg %F -bg %B -D %D -T tight -o %O %f"))
-
-	(dvisvgm :programs ("latex" "dvisvgm")
-		 :description "dvi > svg"
-		 :message
-		 "you need to install the programs: latex and dvisvgm."
-		 :use-xcolor t
-		 :image-input-type "xdv"
-		 :image-output-type "svg"
-		 :image-size-adjust (1.7 . 1.5)
-		 :latex-compiler
-		 ("xelatex -no-pdf -interaction nonstopmode -output-directory %o %f")
-		 :image-converter
-		 ("dvisvgm %f -n -b min -c %S -o %O"))
-
-	(imagemagick
-	 :programs ("latex" "convert")
-	 :description "pdf > png"
-	 :message "you need to install the programs: latex and imagemagick."
-	 :use-xcolor t
-	 :image-input-type "pdf"
-	 :image-output-type "png"
-	 :image-size-adjust (1.5 1.5)
-	 :latex-compiler
-	 ("xelatex -pdf -interaction nonstopmode -output-directory %o %f")
-	 :image-converter
-	 ("convert -density 180 -trim -antialias %f -quality 100 %O")))))
 
 (defun dc/org-theme ()
   (interactive)
@@ -701,9 +698,9 @@ Argument KEY is the bibtex key."
 	  ora-cap-filesystem
 	  org-completion-refs))
   (org-bullets-mode t)
+  (org-edit-latex-mode t)
   (dc/org-theme)
   (variable-pitch-mode)
-  (require 'tex-mode)
   (setq-local prettify-symbols-alist tex--prettify-symbols-alist)
   (setq prettify-symbols-compose-predicate #'prettify-symbols-org-latex-compose-p))
 
